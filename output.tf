1b43c3c817809563e258189226832b71f9dcdae7
tkaburagi <t.kaburagi@me.com>
2022-03-08 10:02:49 +0900

    changed output

--- output.tf
+++ output.tf
@@ -1,8 +1,10 @@
 output "eip" {
+  description = "AWSインスタンスのパブリックIPを表示"
   value = aws_eip.hashicat.public_ip
 }
 
 output "instance-info" {
+  description = "AWSインスタンスの情報を表示"
   value = [
     aws_instance.hashicat.arn,
     aws_instance.hashicat.availability_zone,
