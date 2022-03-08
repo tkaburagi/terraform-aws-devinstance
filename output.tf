77fdb49488e5248cbe6249c4cfcad31571498be7
tkaburagi <t.kaburagi@me.com>
2022-03-08 10:00:38 +0900

    changed output

--- output.tf
+++ output.tf
@@ -1,3 +1,13 @@
-output "name" {
+output "eip" {
   value = aws_eip.hashicat.public_ip
+}
+
+output "instance-info" {
+  value = [
+    aws_instance.hashicat.arn,
+    aws_instance.hashicat.availability_zone,
+    aws_instance.hashicat.cpu_core_count,
+    aws_instance.hashicat.private_ip,
+    aws_instance.hashicat.instance_type
+    ]
 }
